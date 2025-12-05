3de82925e64c137002f24340525d814961029cfb
Luca Colussi <luca.colussi@gmail.com>
2025-12-05 16:35:35 +0100

    feat: add controller textfield and regex email

--- lib/features/auth/pages/login.dart
+++ lib/features/auth/pages/login.dart
@@ -4,8 +4,26 @@ import '../widgets/button.dart';
 import '../widgets/textfield.dart';
 import 'package:greenlinkapp/router.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() {
+    return _LoginPage();
+  }
+}
+
+class _LoginPage extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  String errorMessage = ''; 
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,40 +31,61 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(title: const Text('Login Page')),
 
       body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              AppLogo(),
-              const SizedBox(height: 24),
-              AuthTextField(
-                hint: 'Email',
-                controller: TextEditingController(),
-                obscure: false,
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            AppLogo(),
+            const SizedBox(height: 24),
+            AuthTextField(
+              hint: 'Email',
+              controller: emailController,
+              obscure: false,
+            ),
+            const SizedBox(height: 16),
+            AuthTextField(
+              hint: 'Password',
+              controller: passwordController,
+              obscure: true,
+            ),
+            const SizedBox(height: 16),
+            if (errorMessage.isNotEmpty) ...[
+              Text(
+                errorMessage,
+                style: const TextStyle(color: Colors.red, fontSize: 14),
               ),
               const SizedBox(height: 16),
-              AuthTextField(
-                hint: 'Password',
-                controller: TextEditingController(),
-                obscure: true,
-              ),
-              const SizedBox(height: 16),
-              AuthButton(
-                label: 'Login',
-                textColor: Colors.white,
-                onPressed: () {},
-              ),
-              const SizedBox(height: 16),
-              GestureDetector(
-                onTap: () {
-                  router.push('/register');
-                },
-                child:Text(
+            ],
+            AuthButton(
+              label: 'Login',
+              textColor: Colors.white,
+              onPressed: () {
+                final email = emailController.text.trim();
+                final emailRegex = RegExp(r'^[\w\.-]+@[\w\.-]+\.\w+$');
+
+                if (!emailRegex.hasMatch(email)) {
+                  setState(() {
+                    errorMessage = 'Inserisci un indirizzo email valido';
+                  });
+                  return;
+                }
+                print(emailController.text.trim());
+                print(passwordController.text.trim());
+                emailController.clear();
+                passwordController.clear();
+              },
+            ),
+            const SizedBox(height: 16),
+            GestureDetector(
+              onTap: () {
+                router.go('/register');
+              },
+              child: Text(
                 'Non hai un account? Registrati cliccando qui.',
                 style: TextStyle(fontSize: 16),
               ),
-              ),
-            ],
+            ),
+          ],
         ),
       ),
     );
--- lib/features/auth/pages/register.dart
+++ lib/features/auth/pages/register.dart
@@ -4,63 +4,115 @@ import '../widgets/button.dart';
 import '../widgets/textfield.dart';
 import 'package:greenlinkapp/router.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() {
+    return _RegisterPage();
+  }
+}
+
+class _RegisterPage extends State<RegisterPage> {
+  final nicknameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+  String errorMessage = ''; // ← QUI
+
+  @override
+  void dispose() {
+    nicknameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose(); // pulisco la memoria
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Register Page')),
+      appBar: AppBar(title: const Text('GreenLink')),
 
       body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              AppLogo(),
-              const SizedBox(height: 24),
-              AuthTextField(
-                hint: 'Nickname',
-                controller: TextEditingController(),
-                obscure: false,
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            AppLogo(),
+            const SizedBox(height: 24),
+            AuthTextField(
+              hint: 'Nickname',
+              controller: nicknameController,
+              obscure: false,
+            ),
+            const SizedBox(height: 16),
+            AuthTextField(
+              hint: 'Email',
+              controller: emailController,
+              obscure: false,
+            ),
+            const SizedBox(height: 16),
+            AuthTextField(
+              hint: 'Password',
+              controller: passwordController,
+              obscure: true,
+            ),
+            const SizedBox(height: 16),
+            AuthTextField(
+              hint: 'Confirm Password',
+              controller: confirmPasswordController,
+              obscure: true,
+            ),
+            const SizedBox(height: 16),
+            if (errorMessage.isNotEmpty)...[
+              Text(
+                errorMessage,
+                style: const TextStyle(color: Colors.red, fontSize: 14),
               ),
               const SizedBox(height: 16),
-              AuthTextField(
-                hint: 'Email',
-                controller: TextEditingController(),
-                obscure: false,
-              ),
-              const SizedBox(height: 16),
-              AuthTextField(
-                hint: 'Password',
-                controller: TextEditingController(),
-                obscure: true,
-              ),
-              const SizedBox(height: 16),
-              AuthTextField(
-                hint: 'Confirm Password',
-                controller: TextEditingController(),
-                obscure: true,
-              ),
-              const SizedBox(height: 16),
-              AuthButton(
-                label: 'Registrati',
-                textColor: Colors.white,
-                onPressed: () {},
-              ),
-              const SizedBox(height: 16),
-              GestureDetector(
-                onTap: () {
-                  router.push('/login');
-                },
-                child:Text(
+            ],
+            AuthButton(
+              label: 'Registrati',
+              textColor: Colors.white,
+              onPressed: () {
+                final email = emailController.text.trim();
+                final emailRegex = RegExp(r'^[\w\.-]+@[\w\.-]+\.\w+$');
+
+                if (!emailRegex.hasMatch(email)) {
+                  setState(() {
+                    errorMessage = 'Email non valida';
+                  });
+                  return;
+                }
+                if (passwordController.text.trim() != confirmPasswordController.text.trim()) {
+                  setState(() {
+                    errorMessage = 'Le password non coincidono';
+                  });
+                  return;
+                }
+                print(nicknameController.text);
+                print(emailController.text.trim());
+                print(passwordController.text.trim());
+                print(confirmPasswordController.text.trim());
+                nicknameController.clear();
+                emailController.clear();
+                passwordController.clear();
+                confirmPasswordController.clear();
+              },
+            ),
+            const SizedBox(height: 16),
+            GestureDetector(
+              onTap: () {
+                router.go('/login');
+              },
+              child: Text(
                 'Torna al login cliccando qui.',
                 style: TextStyle(fontSize: 16),
               ),
-              ),
-            ],
-          ),
-        
+            ),
+          ],
+        ),
       ),
     );
   }
